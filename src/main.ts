cc204ee8a3529d41b6cb69c920b8a8fbec0dbb0b
remcohogendorp <historemc.developer@gmail.com>
2023-10-30 11:09:13 +0100

    quick fix audiomap

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
-
+import fs from "fs";
 declare const module: any;
 
 async function bootstrap() {
+  const folderPath = "./audio"; // Replace with your desired folder path
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+  }
+
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
