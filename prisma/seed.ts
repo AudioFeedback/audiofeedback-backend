3c6577c98e56884bdb0df2d114f8490e79e666ca
remcohogendorp <historemc.developer@gmail.com>
2023-11-02 22:12:30 +0100

    user seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,96 @@
+import { PrismaClient, Role, User } from '@prisma/client'
+import { faker, fakerNL } from '@faker-js/faker';
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const musicProducer1 = await createRandomUser(Role.MUZIEKPRODUCER);
+  await addTrackToMusicProducer(musicProducer1, PhonHouseBeat);
+  await addTrackToMusicProducer(musicProducer1, WatrByYourSide);
+  await addTrackToMusicProducer(musicProducer1, FuturisticBeat);
+
+  const musicroducer2 = await createRandomUser(Role.MUZIEKPRODUCER);
+  await addTrackToMusicProducer(musicroducer2, Embrace);
+  await addTrackToMusicProducer(musicroducer2, ModernVlog);
+
+
+  const feedbackgever1 = await createRandomUser(Role.FEEDBACKGEVER);
+  const feedbackgever2= await createRandomUser(Role.FEEDBACKGEVER);
+  const feedbackgever3 = await createRandomUser(Role.FEEDBACKGEVER);
+}
+
+
+function createRandomUser(role?: Role): Promise<User> {
+  const data = {
+    firstname: fakerNL.person.firstName(),
+    lastname: fakerNL.person.lastName(),
+    role: role ? role : faker.helpers.enumValue(Role)
+  }
+
+  return prisma.user.create({data});
+}
+
+function addTrackToMusicProducer(user: User, track: TrackData) {
+  return prisma.track.create({
+    data: {
+      ...track,
+      author: {
+        connect: user
+      }
+    }
+  })
+}
+
+interface TrackData {
+  title: string,
+  genre: string,
+  guid: string,
+  filetype: string
+}
+
+// Zie folder discord, importeer deze in de folder /audio
+
+const PhonHouseBeat = {
+  title: "Phonk House Beat (You Wanna Play)",
+  genre: "Fonk",
+  guid: "phonk-house-beat-you-wanna-play-126321",
+  filetype: "mp3",
+}
+
+const WatrByYourSide = {
+  title: "WatR. - By Your Side",
+  genre: "House",
+  guid: "watr-by-your-side-11516",
+  filetype: "mp3",
+}
+
+const FuturisticBeat = {
+  title: "Futuristic Beat",
+  genre: "Trap",
+  guid: "futuristic-beat-146661",
+  filetype: "mp3",
+}
+
+const Embrace = {
+  title: "Embrace",
+  genre: "Uplifting",
+  guid: "embrace-12278",
+  filetype: "mp3",
+}
+
+const ModernVlog = {
+  title: "Modern Vlog",
+  genre: "Future Bass",
+  guid: "modern-vlog-140795",
+  filetype: "mp3",
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+})
