ac5b0cfa959d8321021f785970c8c892afe54201
remcohogendorp <historemc.developer@gmail.com>
2023-12-07 12:50:23 +0100

    controller opzet

--- src/labels/labels.controller.ts
+++ src/labels/labels.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus, Param, Req, UseGuards } from "@nestjs/common";
 import { LabelsService } from "./labels.service";
+import { Role, User } from "@prisma/client";
+import { GetReviewTrackDto } from "src/tracks/dto/get-review-track.dto";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { Roles } from "src/auth/roles.decorator";
+import { RolesGuard } from "src/auth/roles.guard";
 
+@ApiTags("labels")
 @Controller("labels")
 export class LabelsController {
   constructor(private readonly labelsService: LabelsService) {}
+
+  @Get("invites")
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiBearerAuth()
+  @Roles(Role.FEEDBACKGEVER)
+  async getInvites(@Req() req: Request) {
+
+  }
 }
