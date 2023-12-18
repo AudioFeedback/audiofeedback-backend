31ff8512014ed518594b7f0c677f9a970d59f393
Niek <frankenstein795@gmail.com>
2023-12-18 11:38:31 +0100

    Fix unit tests crashing

--- src/labels/labels.controller.spec.ts
+++ src/labels/labels.controller.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { mockDeep } from "jest-mock-extended";
+import { PrismaService } from "../prisma.service";
+import { UsersService } from "../users/users.service";
 import { LabelsController } from "./labels.controller";
 import { LabelsService } from "./labels.service";
 
@@ -8,8 +11,11 @@ describe("LabelsController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LabelsController],
-      providers: [LabelsService],
-    }).compile();
+      providers: [LabelsService, UsersService, PrismaService],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaService>())
+      .compile();
 
     controller = module.get<LabelsController>(LabelsController);
   });
--- src/labels/labels.service.spec.ts
+++ src/labels/labels.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { mockDeep } from "jest-mock-extended";
+import { PrismaService } from "../prisma.service";
 import { LabelsService } from "./labels.service";
 
 describe("LabelsService", () => {
@@ -6,8 +8,11 @@ describe("LabelsService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LabelsService],
-    }).compile();
+      providers: [LabelsService, PrismaService],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaService>())
+      .compile();
 
     service = module.get<LabelsService>(LabelsService);
   });
