87d0cf4ed7f21cee0c301ceaa6c1f9715471ee51
Niek <frankenstein795@gmail.com>
2024-01-28 19:25:15 +0100

    Add support for hashed passwords in db instead of plaintext

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,17 @@
-import { InviteStatus, Label, PrismaClient, Role, TrackVersion, User } from '@prisma/client'
-import { fakerNL } from '@faker-js/faker';
+import {
+  InviteStatus,
+  Label,
+  PrismaClient,
+  Role,
+  TrackVersion,
+  User,
+} from "@prisma/client";
+import bcrypt from "bcrypt";
+import { fakerNL } from "@faker-js/faker";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-
   // Create label
 
   const LabelSwingTijger = await createLabel(SwingTijger);
@@ -12,142 +19,289 @@ async function main() {
   const LabelLachendeNoten = await createLabel(LachendeNoten);
   const LabelFunkFusie = await createLabel(FunkFusie);
 
-  const superUser = await createRandomUser([Role.MUZIEKPRODUCER, Role.FEEDBACKGEVER, Role.ADMIN], "superuser");
+  const superUser = await createRandomUser(
+    [Role.MUZIEKPRODUCER, Role.FEEDBACKGEVER, Role.ADMIN],
+    "superuser",
+  );
 
   const admin = await createRandomUser([Role.ADMIN], "admin");
-  const feedbackgever1 = await createRandomUser([Role.FEEDBACKGEVER], "feedbackgever");
-  const feedbackgever2= await createRandomUser([Role.FEEDBACKGEVER]);
+  const feedbackgever1 = await createRandomUser(
+    [Role.FEEDBACKGEVER],
+    "feedbackgever",
+  );
+  const feedbackgever2 = await createRandomUser([Role.FEEDBACKGEVER]);
   const feedbackgever3 = await createRandomUser([Role.FEEDBACKGEVER]);
 
+  const musicProducer1 = await createRandomUser(
+    [Role.MUZIEKPRODUCER],
+    "muziekproducer",
+  );
 
-  const musicProducer1 = await createRandomUser([Role.MUZIEKPRODUCER], "muziekproducer");
+  const PerfectNight = await addTrackToMusicProducer(
+    musicProducer1,
+    PefectNight,
+    [feedbackgever1, feedbackgever3],
+  );
 
-  const PerfectNight = await addTrackToMusicProducer(musicProducer1, PefectNight, [feedbackgever1, feedbackgever3]);
+  await addTrackToLabel(PerfectNight[0], LabelSwingTijger);
+  await addTrackToLabel(PerfectNight[0], LabelBananenBeat);
+  await addTrackToLabel(PerfectNight[0], LabelLachendeNoten);
+  await addTrackToLabel(PerfectNight[0], LabelFunkFusie);
 
-  await addTrackToLabel(PerfectNight[0], LabelSwingTijger)
-  await addTrackToLabel(PerfectNight[0], LabelBananenBeat)
-  await addTrackToLabel(PerfectNight[0], LabelLachendeNoten)
-  await addTrackToLabel(PerfectNight[0], LabelFunkFusie)
+  const WatrByYourSideTrack = await addTrackToMusicProducer(
+    musicProducer1,
+    WatrByYourSide,
+    [feedbackgever1, feedbackgever2, feedbackgever3],
+  );
 
-  const WatrByYourSideTrack = await addTrackToMusicProducer(musicProducer1, WatrByYourSide, [feedbackgever1, feedbackgever2, feedbackgever3]);
+  await addTrackToLabel(WatrByYourSideTrack[0], LabelSwingTijger);
+  await addTrackToLabel(WatrByYourSideTrack[0], LabelBananenBeat);
+  await addTrackToLabel(WatrByYourSideTrack[0], LabelLachendeNoten);
+  await addTrackToLabel(WatrByYourSideTrack[0], LabelFunkFusie);
 
-  await addTrackToLabel(WatrByYourSideTrack[0], LabelSwingTijger)
-  await addTrackToLabel(WatrByYourSideTrack[0], LabelBananenBeat)
-  await addTrackToLabel(WatrByYourSideTrack[0], LabelLachendeNoten)
-  await addTrackToLabel(WatrByYourSideTrack[0], LabelFunkFusie)
+  const FuturisticBeatTrack = await addTrackToMusicProducer(
+    musicProducer1,
+    FuturisticBeat,
+    [feedbackgever1, feedbackgever2, feedbackgever3],
+  );
 
-  const FuturisticBeatTrack = await addTrackToMusicProducer(musicProducer1, FuturisticBeat, [feedbackgever1, feedbackgever2, feedbackgever3]);
-
-  await addTrackToLabel(FuturisticBeatTrack[0], LabelSwingTijger)
-  await addTrackToLabel(FuturisticBeatTrack[0], LabelBananenBeat)
-  await addTrackToLabel(FuturisticBeatTrack[0], LabelLachendeNoten)
-  await addTrackToLabel(FuturisticBeatTrack[0], LabelFunkFusie)
+  await addTrackToLabel(FuturisticBeatTrack[0], LabelSwingTijger);
+  await addTrackToLabel(FuturisticBeatTrack[0], LabelBananenBeat);
+  await addTrackToLabel(FuturisticBeatTrack[0], LabelLachendeNoten);
+  await addTrackToLabel(FuturisticBeatTrack[0], LabelFunkFusie);
 
   const musicroducer2 = await createRandomUser([Role.MUZIEKPRODUCER]);
 
-  const EmbraceTrack = await addTrackToMusicProducer(musicroducer2, Embrace, [feedbackgever1, feedbackgever2, feedbackgever3]);
+  const EmbraceTrack = await addTrackToMusicProducer(musicroducer2, Embrace, [
+    feedbackgever1,
+    feedbackgever2,
+    feedbackgever3,
+  ]);
 
-  await addTrackToLabel(EmbraceTrack[0], LabelSwingTijger)
-  await addTrackToLabel(EmbraceTrack[0], LabelBananenBeat)
-  await addTrackToLabel(EmbraceTrack[0], LabelLachendeNoten)
-  await addTrackToLabel(EmbraceTrack[0], LabelFunkFusie)
+  await addTrackToLabel(EmbraceTrack[0], LabelSwingTijger);
+  await addTrackToLabel(EmbraceTrack[0], LabelBananenBeat);
+  await addTrackToLabel(EmbraceTrack[0], LabelLachendeNoten);
+  await addTrackToLabel(EmbraceTrack[0], LabelFunkFusie);
 
-  const ModernVlogTrack = await addTrackToMusicProducer(musicroducer2, ModernVlog, [feedbackgever3]);
+  const ModernVlogTrack = await addTrackToMusicProducer(
+    musicroducer2,
+    ModernVlog,
+    [feedbackgever3],
+  );
 
-  await addUserToLabel(InviteStatus.ACCEPTED, superUser, LabelSwingTijger)
-  await addUserToLabel(InviteStatus.ACCEPTED, superUser, LabelBananenBeat)
-  await addUserToLabel(InviteStatus.ACCEPTED, superUser, LabelLachendeNoten)
-  await addUserToLabel(InviteStatus.ACCEPTED, superUser, LabelFunkFusie)
+  await addUserToLabel(InviteStatus.ACCEPTED, superUser, LabelSwingTijger);
+  await addUserToLabel(InviteStatus.ACCEPTED, superUser, LabelBananenBeat);
+  await addUserToLabel(InviteStatus.ACCEPTED, superUser, LabelLachendeNoten);
+  await addUserToLabel(InviteStatus.ACCEPTED, superUser, LabelFunkFusie);
 
-  await addUserToLabel(InviteStatus.ACCEPTED, admin, LabelSwingTijger)
-  await addUserToLabel(InviteStatus.ACCEPTED, admin, LabelBananenBeat)
-  await addUserToLabel(InviteStatus.ACCEPTED, admin, LabelLachendeNoten)
-  await addUserToLabel(InviteStatus.ACCEPTED, admin, LabelFunkFusie)
+  await addUserToLabel(InviteStatus.ACCEPTED, admin, LabelSwingTijger);
+  await addUserToLabel(InviteStatus.ACCEPTED, admin, LabelBananenBeat);
+  await addUserToLabel(InviteStatus.ACCEPTED, admin, LabelLachendeNoten);
+  await addUserToLabel(InviteStatus.ACCEPTED, admin, LabelFunkFusie);
 
-  await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever1, LabelSwingTijger)
-  await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever1, LabelBananenBeat)
-  await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever1, LabelLachendeNoten)
-  await addUserToLabel(InviteStatus.INVITED, feedbackgever1, LabelFunkFusie)
+  await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever1, LabelSwingTijger);
+  await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever1, LabelBananenBeat);
+  await addUserToLabel(
+    InviteStatus.ACCEPTED,
+    feedbackgever1,
+    LabelLachendeNoten,
+  );
+  await addUserToLabel(InviteStatus.INVITED, feedbackgever1, LabelFunkFusie);
 
-  await addUserToLabel(InviteStatus.INVITED, feedbackgever2, LabelSwingTijger)
-  await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever2, LabelBananenBeat)
-  await addUserToLabel(InviteStatus.INVITED, feedbackgever2, LabelLachendeNoten)
-  await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever2, LabelFunkFusie)
+  await addUserToLabel(InviteStatus.INVITED, feedbackgever2, LabelSwingTijger);
+  await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever2, LabelBananenBeat);
+  await addUserToLabel(
+    InviteStatus.INVITED,
+    feedbackgever2,
+    LabelLachendeNoten,
+  );
+  await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever2, LabelFunkFusie);
 
   // await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever3, LabelSwingTijger)
   // await addUserToLabel(InviteStatus.INVITED, feedbackgever3, LabelBananenBeat)
   // await addUserToLabel(InviteStatus.ACCEPTED, feedbackgever3, LabelLachendeNoten)
   // await addUserToLabel(InviteStatus.INVITED, feedbackgever3, LabelFunkFusie)
 
-  await addFeedbackToTrack(createRandomFeedback(), PerfectNight[0], feedbackgever1)
-  await addFeedbackToTrack(createRandomFeedback(), PerfectNight[1], feedbackgever1)
-  await addFeedbackToTrack(createRandomFeedback(), PerfectNight[1], feedbackgever1)
-  await addFeedbackToTrack(createRandomFeedback(), PerfectNight[1], feedbackgever1)
-  await addFeedbackToTrack(createRandomFeedback(), WatrByYourSideTrack[0], feedbackgever1)
-  await addFeedbackToTrack(createRandomFeedback(), WatrByYourSideTrack[1], feedbackgever1)
-  await addFeedbackToTrack(createRandomFeedback(), FuturisticBeatTrack[0], feedbackgever1)
-  await addFeedbackToTrack(createRandomFeedback(), FuturisticBeatTrack[1], feedbackgever1)
-  await addFeedbackToTrack(createRandomFeedback(), EmbraceTrack[0], feedbackgever1)
-  await addFeedbackToTrack(createRandomFeedback(), EmbraceTrack[1], feedbackgever1)
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    PerfectNight[0],
+    feedbackgever1,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    PerfectNight[1],
+    feedbackgever1,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    PerfectNight[1],
+    feedbackgever1,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    PerfectNight[1],
+    feedbackgever1,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    WatrByYourSideTrack[0],
+    feedbackgever1,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    WatrByYourSideTrack[1],
+    feedbackgever1,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    FuturisticBeatTrack[0],
+    feedbackgever1,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    FuturisticBeatTrack[1],
+    feedbackgever1,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    EmbraceTrack[0],
+    feedbackgever1,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    EmbraceTrack[1],
+    feedbackgever1,
+  );
 
-  await addFeedbackToTrack(createRandomFeedback(), FuturisticBeatTrack[0], feedbackgever2)
-  await addFeedbackToTrack(createRandomFeedback(), FuturisticBeatTrack[1], feedbackgever2)
-  await addFeedbackToTrack(createRandomFeedback(), EmbraceTrack[0], feedbackgever2)
-  await addFeedbackToTrack(createRandomFeedback(), EmbraceTrack[1], feedbackgever2)
-  await addFeedbackToTrack(createRandomFeedback(), WatrByYourSideTrack[0], feedbackgever2)
-  await addFeedbackToTrack(createRandomFeedback(), WatrByYourSideTrack[1], feedbackgever2)
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    FuturisticBeatTrack[0],
+    feedbackgever2,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    FuturisticBeatTrack[1],
+    feedbackgever2,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    EmbraceTrack[0],
+    feedbackgever2,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    EmbraceTrack[1],
+    feedbackgever2,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    WatrByYourSideTrack[0],
+    feedbackgever2,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    WatrByYourSideTrack[1],
+    feedbackgever2,
+  );
 
-
-  await addFeedbackToTrack(createRandomFeedback(), PerfectNight[0], feedbackgever3)
-  await addFeedbackToTrack(createRandomFeedback(), PerfectNight[1], feedbackgever3)
-  await addFeedbackToTrack(createRandomFeedback(), WatrByYourSideTrack[0], feedbackgever3)
-  await addFeedbackToTrack(createRandomFeedback(), WatrByYourSideTrack[1], feedbackgever3)
-  await addFeedbackToTrack(createRandomFeedback(), FuturisticBeatTrack[0], feedbackgever3)
-  await addFeedbackToTrack(createRandomFeedback(), FuturisticBeatTrack[1], feedbackgever3)
-  await addFeedbackToTrack(createRandomFeedback(), EmbraceTrack[0], feedbackgever3)
-  await addFeedbackToTrack(createRandomFeedback(), EmbraceTrack[1], feedbackgever3)
-  await addFeedbackToTrack(createRandomFeedback(), ModernVlogTrack[0], feedbackgever3)
-  await addFeedbackToTrack(createRandomFeedback(), ModernVlogTrack[1], feedbackgever3)
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    PerfectNight[0],
+    feedbackgever3,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    PerfectNight[1],
+    feedbackgever3,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    WatrByYourSideTrack[0],
+    feedbackgever3,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    WatrByYourSideTrack[1],
+    feedbackgever3,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    FuturisticBeatTrack[0],
+    feedbackgever3,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    FuturisticBeatTrack[1],
+    feedbackgever3,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    EmbraceTrack[0],
+    feedbackgever3,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    EmbraceTrack[1],
+    feedbackgever3,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    ModernVlogTrack[0],
+    feedbackgever3,
+  );
+  await addFeedbackToTrack(
+    createRandomFeedback(),
+    ModernVlogTrack[1],
+    feedbackgever3,
+  );
 }
 
-
-function createRandomUser(roles: Role[], username?: string): Promise<User> {
+async function createRandomUser(
+  roles: Role[],
+  username?: string,
+): Promise<User> {
   const data = {
     username: username ? username : fakerNL.internet.userName(),
     firstname: fakerNL.person.firstName(),
-    password: "demo123",
+    password: await bcrypt.hash("demo123", 10),
     lastname: fakerNL.person.lastName(),
     roles: roles,
-  }
+  };
 
-  return prisma.user.create({data});
+  return prisma.user.create({ data });
 }
 
 function createRandomFeedback(): FeedbackData {
   return {
     rating: fakerNL.datatype.boolean(),
-    comment: fakerNL.lorem.lines({min: 1, max: 1}),
-    timestamp: fakerNL.number.float({min: 0.1, precision: 0.01 }),
+    comment: fakerNL.lorem.lines({ min: 1, max: 1 }),
+    timestamp: fakerNL.number.float({ min: 0.1, precision: 0.01 }),
     isPublished: true,
-  }
+  };
 }
 
-async function addTrackToMusicProducer(user: User, trackData: TrackData, reviewers: User[]) {
-
-  const {title, genre, versionNumber, description, guid, filetype, duration} = trackData;
+async function addTrackToMusicProducer(
+  user: User,
+  trackData: TrackData,
+  reviewers: User[],
+) {
+  const { title, genre, versionNumber, description, guid, filetype, duration } =
+    trackData;
 
   const track = await prisma.track.create({
     data: {
       title: title,
       genre: genre,
       reviewers: {
-        connect: reviewers.map(x => { return {username: x.username}}),
+        connect: reviewers.map((x) => {
+          return { username: x.username };
+        }),
       },
       author: {
-        connect: {username: user.username}
-      }
-    }
-  })
+        connect: { username: user.username },
+      },
+    },
+  });
 
   const firstTrackVersion = await prisma.trackVersion.create({
     data: {
@@ -157,10 +311,10 @@ async function addTrackToMusicProducer(user: User, trackData: TrackData, reviewe
       filetype: filetype,
       duration: duration,
       track: {
-        connect: track
-      }
-    }
-  })
+        connect: track,
+      },
+    },
+  });
 
   const secondTrackVersion = await prisma.trackVersion.create({
     data: {
@@ -170,26 +324,30 @@ async function addTrackToMusicProducer(user: User, trackData: TrackData, reviewe
       filetype: filetype,
       duration: duration,
       track: {
-        connect: track
-      }
-    }
-  })
-  
+        connect: track,
+      },
+    },
+  });
+
   return [firstTrackVersion, secondTrackVersion];
 }
 
-function addFeedbackToTrack(feedback: FeedbackData, track: TrackVersion, user: User) {
+function addFeedbackToTrack(
+  feedback: FeedbackData,
+  track: TrackVersion,
+  user: User,
+) {
   return prisma.feedback.create({
     data: {
       ...feedback,
       trackVersion: {
-        connect: track
+        connect: track,
       },
       user: {
-        connect: {username: user.username}
-      }
-    }
-  })
+        connect: { username: user.username },
+      },
+    },
+  });
 }
 
 function addUserToLabel(status: InviteStatus, user: User, label: Label) {
@@ -203,19 +361,19 @@ function addUserToLabel(status: InviteStatus, user: User, label: Label) {
       },
       user: {
         connect: {
-          id: user.id
+          id: user.id,
         },
-      }
-    }
-  })
+      },
+    },
+  });
 }
 
 function createLabel(label: LabelData) {
   return prisma.label.create({
     data: {
-      ...label
-    }
-  })
+      ...label,
+    },
+  });
 }
 
 async function addTrackToLabel(trackVersion: TrackVersion, label: Label) {
@@ -224,21 +382,21 @@ async function addTrackToLabel(trackVersion: TrackVersion, label: Label) {
       trackVersions: {
         some: {
           id: trackVersion.id,
-        }
-      }
-    }
-  })
+        },
+      },
+    },
+  });
 
   return prisma.track.update({
     where: {
-      id: track.id
+      id: track.id,
     },
     data: {
       label: {
-        connect: {id: label.id}
-      }
-    }
-  })
+        connect: { id: label.id },
+      },
+    },
+  });
 }
 
 interface TrackData {
@@ -275,7 +433,7 @@ const PefectNight: TrackData = {
   versionNumber: 1,
   description: "Eerste versie van de track.",
   duration: 162,
-}
+};
 
 const WatrByYourSide: TrackData = {
   title: "WatR. - By Your Side",
@@ -285,7 +443,7 @@ const WatrByYourSide: TrackData = {
   versionNumber: 1,
   description: "Eerste versie van de track.",
   duration: 194,
-}
+};
 
 const FuturisticBeat: TrackData = {
   title: "Futuristic Beat",
@@ -295,7 +453,7 @@ const FuturisticBeat: TrackData = {
   versionNumber: 1,
   description: "Eerste versie van de track.",
   duration: 121,
-}
+};
 
 const Embrace: TrackData = {
   title: "Embrace",
@@ -305,7 +463,7 @@ const Embrace: TrackData = {
   versionNumber: 1,
   description: "Eerste versie van de track.",
   duration: 179,
-}
+};
 
 const ModernVlog: TrackData = {
   title: "Modern Vlog",
@@ -315,42 +473,42 @@ const ModernVlog: TrackData = {
   versionNumber: 1,
   description: "Eerste versie van de track.",
   duration: 139,
-}
+};
 
 const SwingTijger: LabelData = {
   name: "Swing Tijger",
   websiteUrl: "https://swingtijger.nl",
   description: "Jazz voor katachtige dansmoves.",
   genre: "Jazzhop",
-}
+};
 
 const BananenBeat: LabelData = {
   name: "Bananen Beat",
   websiteUrl: "https://bananenbeat.nl",
   description: "Rijpe beats voor een tropische sfeer.",
   genre: "Tropische House",
-}
+};
 
 const LachendeNoten: LabelData = {
   name: "Lachende Noten",
   websiteUrl: "https://lachendenoten.nl",
   description: "Muziek die een glimlach op je gezicht tovert.",
   genre: "Feelgood Pop",
-}
+};
 
 const FunkFusie: LabelData = {
   name: "Funk Fusie",
   websiteUrl: "https://funkfusie.nl",
   description: "Smeltkroes van funky geluiden.",
   genre: "Eclectische Funk",
-}
+};
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-})
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import bcrypt from "bcrypt";
 import { UsersService } from "src/users/users.service";
 
 @Injectable()
@@ -11,7 +12,10 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne({ username: username });
-    if (user && user.password === pass) {
+
+    const auth = await bcrypt.compare(pass, user.password);
+
+    if (user && auth) {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user;
       return result;
